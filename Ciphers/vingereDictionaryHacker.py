12661b311a57b07a2dc1a3a6614fe5c29669fde7
Akinlade Abdulsamad gbolahan <59762970+kinsomaz@users.noreply.github.com>
2020-10-13 12:41:55 +0100

    Add files via upload

---
+++ Ciphers/vingereDictionaryHacker.py
@@ -0,0 +1,35 @@
+import detectENGLISH, vigenereCipher, pyperclip
+def main():
+    ciphertext = """Tzx isnz eccjxkg nfq lol mys bbqq I lxcz."""
+    hackedMessage = hackVigenere(ciphertext)
+    if hackedMessage != None:
+        print('Copying hacked message to clipboard:')
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+    else:
+        print('Failed to hack encryption.')
+
+def hackVigenere(ciphertext):
+    fo = open('dictionary.txt')
+    words = fo.readlines()
+    fo.close()
+    for word in words:
+        word = word.strip()
+        decryptedText = vigenereCipher.decryptMessage(word, ciphertext) 
+        if detectENGLISH.isEnglish(decryptedText, wordPercentage=40):
+            print()
+            print('Possible encryption break:')
+            print('Key ' + str(word) + ': ' + decryptedText[:100])
+            print()
+            print('Enter D for done, or just press Enter to continue breaking:')
+            response = input('> ')
+            if response.upper().startswith('D'):
+                return decryptedText
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+    
